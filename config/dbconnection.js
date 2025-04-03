49b4ee6bf3c8fcd757799a90e44768a74d5f63f7
Dhruvi Gohil <gohildhruvi168529@gmail.com>
2025-04-03 16:51:02 +0530

    admin-panel-passport

--- config/dbconnection.js
+++ config/dbconnection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const dbconnect = () => {
-    mongoose.connect("mongodb+srv://dhruvi1685:dhruvi123@cluster0.mw1ii.mongodb.net/Admin-panel-Passport")
+    mongoose.connect("mongodb+srv://dhruvi1685:dhruvi123@cluster0.mw1ii.mongodb.net/Admin-panel-passport")
     .then(() => console.log("Database connected successfully..."))
     .catch(err => console.error("Database Connection Error:", err));
 };
